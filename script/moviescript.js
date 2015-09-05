d72dc17f81873bce6dd9c1a27268b59552b6e814
Ken Bonny <bonny.ken@gmail.com>
2015-09-05 16:41:41 +0200

    First checkin.
    Checking in for the first time.

---
+++ script/moviescript.js
@@ -0,0 +1,18 @@
+﻿$.getJSON("data.json", function(data) {
+    var movies = data.movies.sort(function (a, b) { return a.order - b.order });
+    var template = $("#movie-template").html();
+    var inverted = $("#movie-template-inverted").html();
+
+    $.each(movies, function(i, movie) {
+        var rendered;
+        if (i % 2 === 0) {
+            rendered = Mustache.render(template, movie);
+        } else {
+            rendered = Mustache.render(inverted, movie);
+        }
+
+        $("#movies").append(rendered);
+    });
+}).fail(function(error) {
+    console.log(error);
+});
